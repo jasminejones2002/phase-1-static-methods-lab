8d8d24665cec24c37457d08b44559f43f46ab59f
jasminejones2002 <jazziejones02@yahoo.com>
2024-01-16 05:34:40 -0600

    static mehtods

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    if (string.length === 0) {
+      return string;
+    }
+    const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
+    return capitalizedString;
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]/g, '')
+  }
+
+  static titleize(string) {
+    const excludedWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = string.split(' ');
+
+    const titleizedWords = words.map((word, index) => {
+      if (index === 0 || !excludedWords.includes(word)) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      } else {
+        return word;
+      }
+    });
+
+    return titleizedWords.join(' ');
+  }
 }
